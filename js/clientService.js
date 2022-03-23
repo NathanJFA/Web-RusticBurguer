de443e805eadd91d4f3de143f41f0102f67fafc8
nathanjfa <nathan.farias@dcx.ufpb.br>
2022-03-23 19:01:53 -0300

    finalizando o service pra registrar client

---
+++ js/clientService.js
@@ -0,0 +1,39 @@
+
+
+function registerClient(){
+    const name = Document.getElementById('name').value
+    const lastname = Document.getElementById('lastname').value
+    const email = Document.getElementById('lastname').value
+    const sexo = Document.getElementById('sexo').value
+    const password = Document.getElementById('password').value
+    const passwordVerification = Document.getElementById('password2').value
+    const day = Document.getElementById('day').value
+    const month = Document.getElementById('month').value
+    const year = Document.getElementById('year').value
+    const date = day + '-' + month + '-' + year
+
+    
+    if(validade(password, passwordVerification)){
+        try{
+            const body = JSON.stringify({
+                name: this.name,
+                lastname: this.lastname,
+                email: this.email,
+                sexo: this.sexo,
+                password: this.password,
+                date: this.date
+            })
+        const headers = {'Content-Type':'application/json'}
+        fetch('10.52.5.181:3005/api/clients/', { method: 'POST', body: corpo, headers: cabecalhos}).then(console.log) 
+        }
+        catch(erro){
+            console.log({message: erro.message})
+        } 
+    }
+}
+function validate(password, passwordValidation){
+    if(password === passwordValidation){
+        return true
+    }
+    return false
+}
