0b241b20255c684ae6d0dc2e65d68fa072cbe910
nathanjfa <nathan.farias@dcx.ufpb.br>
2022-03-23 17:02:32 -0300

    iniciando a integraçao

--- Layout 1/valida_acesso.js
+++
@@ -1,9 +0,0 @@
-<?php
-
-#verifica se o CLIENTE está autenticado -> prevenção de acesso indevido
-session_start();
-if(!isset($_SESSION['autenticado']) || $_SESSION['autenticado'] != 'true'){
-    header('Location: ../login_cliente.php?login_cliente=erro_acesso_indevido');
-}
-
-?>
